fe2f0065e4b38a0efb065abead73eb2459b4ab7e
Ryan Ford <ryanford-dev@users.noreply.github.com>
2020-06-19 04:14:06 +0800

    support orphaned elements as fragments

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 #### Like an `h` function but constructs real DOM trees
 
-Small (373b) helper function to make working with HTML in JS a little easier.
+Small (509b) helper function to make working with HTML in JS a little easier.
 
 #### Usage:
 
--- src/lfn.js
+++ src/lfn.js
@@ -1,26 +1,35 @@
 export const l = (name, props, ...rest) => {
 	const children = [];
+	const nametype = typeof name;
+	const contains_attributes = typeof props === "object" && !(props instanceof Node);
 	let el;
+	let template = false;
 
-	if (name === "svg") {
+	if (nametype !== "string") {
+		el = document.createDocumentFragment();
+		template = true;
+		if (name instanceof Node) {
+			children.push(name);
+		}
+	} else	if (name === "svg") {
 		el = document.createElementNS("http://www.w3.org/2000/svg", name);
 	} else {
 		el = document.createElement(name);
 	}
 
-	if (typeof props === "object") {
-		for (const prop in props) {
-			el.setAttribute(prop, props[prop]);
+	if (props) {
+		if (contains_attributes && !template) {
+			Object.keys(props).forEach(prop => el.setAttribute(prop, props[prop]));
+		} else {
+			children.push(props);
 		}
-	} else if (props) {
-		children[0] = props;
 	}
 
 	children.push(...rest);
 
-	children.forEach(node => {
-		const fn = typeof node === "string" ? "createTextNode" : "createElement";
-		el.appendChild(node instanceof Node ? node : document[fn](node));
+	children.forEach(child => {
+		const fn = typeof child === "string" ? "createTextNode" : "createElement";
+		el.appendChild(child instanceof Node ? child : document[fn](child));
 	});
 
 	return el;
